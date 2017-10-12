32148335940df56db507ade05bc5da6a88ee044e
user1 <user1@user1noMacBook-Pro.local>
2017-10-12 18:02:37 +0900

    add collection_select in modal

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -2,11 +2,11 @@ class TradesController < ApplicationController
 
   def index
     @tradeinfos = Trade.all
+    @tokens = Token.new
   end
 
   def new
     @tradeinfo = Trade.new
-
   end
 
   def create
