9ab02bd30145c70938da4af0d33a05298ed1343f
user1 <user1@user1noMacBook-Pro.local>
2017-10-13 11:20:20 +0900

    add BUY and Cancel Button

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,10 +1,12 @@
 class TradesController < ApplicationController
   require "#{Dir.pwd}/app/models/EthereumAPI.rb"
+  before_action :set_trade, only: [:update, :destroy]
 
   def index
     @trades = Trade.all
     @tokens = Token.new
     @trade = Trade.new
+    @trade_amount = Trade.new
   end
 
   def new
@@ -12,19 +14,37 @@ class TradesController < ApplicationController
 
   def create
     @trade = Trade.new(trade_params)
-    @trade.from_token_name =  Token.find(@trade.from_token_name.to_i).symbol
-    binding.pry
-    @trade.to_token_name =  Token.find(@trade.to_token_name.to_i).symbol
-    @trade.to_token_amount = @trade.price * @trade.from_token_amount
-    if @trade.save
-      redirect_to trades_path, notice: "Success Sale's Info Set!"
+    if @trade.from_token_name == @trade.to_token_name
+      redirect_to trades_path, notice: "Token types are the same!"
     else
-      render 'new'
+      @trade.from_token_name =  Token.find(@trade.from_token_name.to_i).symbol
+      @trade.to_token_name =  Token.find(@trade.to_token_name.to_i).symbol
+      @trade.to_token_amount = @trade.price * @trade.from_token_amount
+      @trade.maker_address = current_user.username
+      if @trade.save
+        redirect_to trades_path, notice: "Success Sale's Info Set!"
+      else
+        render 'new'
+      end
     end
   end
+
+  def update
+    puts "update"
+  end
+
+  def destroy
+    @trade.destroy
+    redirect_to trades_path, notice: "Success Cancel"
+  end
+
 
   private
     def trade_params
       params.require(:trade).permit(:price, :from_token_name, :to_token_name, :from_token_amount, :to_token_amount, :maker_address).to_h
+    end
+
+    def set_trade
+      @trade = Trade.find(params[:id])
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tokens, only: [:index, :new, :create]
 
-  resources :trades, only: [:index, :new, :create]
+  resources :trades, only: [:index, :new, :create, :update, :destroy]
+
+
 
   root 'top#index'
 
