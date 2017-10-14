463ebd46f11ffa4b26ca4e8ed9a6ef04a128236a
shiki-tak <shiki.tak@gmail.com>
2017-10-14 21:45:36 +0900

    ISSUE: 一度ロックがかかると解除できない

--- app/models/ethereumAPI.rb
+++ app/models/ethereumAPI.rb
@@ -3,27 +3,20 @@ class EthereumAPI
   PATH = "#{Dir.pwd}/contracts/ERC20Token.sol"
   require "#{Dir.pwd}/app/models/hasheduser.rb"
   require "json"
+  # SUPPLIER_ADDRESS = "0xd4232bdbc4cdbdc9d131103de55b9a2b68d45c78" # office mac book air
+  SUPPLIER_ADDRESS = "0xd34da9604e5e9c2a9cc0aa481b6b24a72af3253b" # private mac book air
+  SUPPLIER_PASSWORD = "pass0"
 
   def set_client(owner_address, ether_account_password)
     # TODO: Error handling when executing Smart Contract
-    # ISSUE: gethを入れて最初に作ったアカウント(eth.accounts[0], "pass0")ではコントラクトが実行できるが
-    #        sign_upで登録したuserでは"IOError - account is locked:"のエラーが発生して
-    #        コントラクトが実行できない。
-
-    # owner_address = "0xd4232bdbc4cdbdc9d131103de55b9a2b68d45c78" # office mac book air
-    owner_address = "0xd34da9604e5e9c2a9cc0aa481b6b24a72af3253b" # private mac book air
-    ether_account_password = "pass0"
     @client = Ethereum::HttpClient.new('http://localhost:8545')
     @client.personal_unlock_account(owner_address, ether_account_password)
-    @contract = Ethereum::Contract.create(file: PATH, client: @client)
-  end
-
-  # Send ether for gas to users registered for demonstration
-  def sendEtherForGas
   end
 
   def deployERC20Token(owner_address, ether_account_password, name, symbol, totalTokens)
     set_client(owner_address, ether_account_password)
+    @contract = Ethereum::Contract.create(file: PATH, client: @client)
+    # ISSUE: 一度ロックがかかると解除できない ⇒ contractのdeployに失敗する
     # smart contractのデプロイ時にtotalTokensの型がfloatではエラーが起きるためinteger型にキャストした
     @address = @contract.deploy_and_wait(owner_address, name, symbol, totalTokens.to_i)
     puts @contract.call.balance_of(owner_address)
@@ -31,6 +24,7 @@ class EthereumAPI
 
   # execute trade
   def executeTransfer(maker_address, to_username, amount, password)
+    @contract = Ethereum::Contract.create(file: PATH, client: @client)
     @address = @contract.transact_and_wait.transfer_from(maker_address, to_username, amount)
   end
 
@@ -40,8 +34,21 @@ class EthereumAPI
     createAccountAtJSON = JSON.generate(createAccount)
     createAccountResult = JSON.parse(createAccountAtJSON)
     etherAccount = createAccountResult['result'].to_s
+    sendEtherForGas(etherAccount)
     @client.personal_unlock_account(etherAccount, password)
+    @contract = Ethereum::Contract.create(file: PATH, client: @client)
     puts "createGethAccount Result: #{etherAccount}"
     return etherAccount
+  end
+
+  # Send ether for gas to users registered for demonstration
+  def sendEtherForGas(etherAccount)
+    set_client(SUPPLIER_ADDRESS, SUPPLIER_PASSWORD)
+    @client.eth_send_transaction({
+      "from": SUPPLIER_ADDRESS,
+      "to": etherAccount,
+      "value": "0x64"
+      })
+      puts "Success send ether from #{SUPPLIER_ADDRESS} to #{etherAccount}"
   end
 end
